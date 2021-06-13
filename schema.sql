31a9f717a3e8e799f21b0879f594ed37a5e0cc03
ashok parihar <aparihar@email.arizona.edu>
2021-06-12 23:39:19 -0600

    Adding corrected schema.sql file.

--- schema.sql
+++ schema.sql
@@ -35,18 +35,9 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title VARCHAR NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-
 CREATE TABLE dept_emp (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -54,4 +45,18 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title VARCHAR (40) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, title, from_date)
+);
+
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM dept_manager;
+SELECT * FROM titles;
+SELECT * FROM dept_emp;
